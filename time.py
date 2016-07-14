24b00f7f3e871fe23d68e2cdce1f5f9b69d39fd8
Yau Ref <yau.ref@gmail.com>
2016-07-14 17:56:11 +0300

    implement basic parsing

--- time.py
+++ time.py
@@ -1,12 +1,34 @@
 #! /usr/bin/python3
+from collections import namedtuple
+Task = namedtuple('Task', ['name', 'timeMarks', 'issues'])
 
-block = False
-blocks = []
+def parseTask(task):
+  split = task.split('-')
+  taskName = split.pop().strip()
+  timeMarks = [timeMark.strip() for item in split for timeMark in item.split(',')]
+  return Task(taskName, timeMarks, [])
 
+def startNewDay(day, days):
+  if day is not None:
+    days.append(day)  
+  return []
+
+
+days = []
+day = None
 with open('time.md', 'r') as file:
   for line in file:
-    item = line 
-    blocks.append(item)
+    if line.startswith('#'):
+      day = startNewDay(day, days)
+    elif line.strip():
+      if not line.startswith(' '):
+        day.append(parseTask(line))        
+      else:
+        day[-1].issues.append(line.strip())
+        
 
-for i in blocks:
-  print(i)
+for day in days:
+  for task in day:
+    print(task.name)
+    print(task.timeMarks)
+    print(task.issues)
