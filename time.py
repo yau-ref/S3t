42d66412783e90159581a21577586d04cb7e905a
Yau Ref <yau.ref@gmail.com>
2016-07-16 11:07:18 +0300

    implement Task as class instead of named tuple to let group its methods

--- time.py
+++ time.py
@@ -1,12 +1,19 @@
 #! /usr/bin/python3
-from collections import namedtuple
-Task = namedtuple('Task', ['name', 'timeMarks', 'issues'])
 
-def parseTask(task):
-  split = task.split('-')
-  taskName = split.pop().strip()
-  timeMarks = [timeMark.strip() for item in split for timeMark in item.split(',')]
-  return Task(taskName, timeMarks, [])
+class Task(object):
+  
+  def __init__(self, name, timeMarks, issues = None):
+    self.name = name
+    self.timeMarks = timeMarks
+    self.issues = issues if issues is not None else []
+  
+  def fromLine(line):
+    split = line.split('-')
+    taskName = split.pop().strip()
+    timeMarks = [timeMark.strip() for item in split for timeMark in item.split(',')]
+    return Task(taskName, timeMarks)
+    
+  def totalTime
 
 def startNewDay(day, days):
   if day is not None:
@@ -22,13 +29,14 @@ with open('time.md', 'r') as file:
       day = startNewDay(day, days)
     elif line.strip():
       if not line.startswith(' '):
-        day.append(parseTask(line))        
+        day.append(Task.fromLine(line)) 
       else:
         day[-1].issues.append(line.strip())
-        
+days.append(day)
 
 for day in days:
   for task in day:
     print(task.name)
     print(task.timeMarks)
     print(task.issues)
+  print('===================')
