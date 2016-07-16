3bc3703b07a668eda15351aae684786fc9207d80
Yau Ref <yau.ref@gmail.com>
2016-07-17 02:19:35 +0300

    implement total task time counting

--- time.py
+++ time.py
@@ -1,4 +1,5 @@
 #! /usr/bin/python3
+from datetime import datetime
 
 class Task(object):
   
@@ -6,20 +7,28 @@ class Task(object):
     self.name = name
     self.timeMarks = timeMarks
     self.issues = issues if issues is not None else []
-  
+    self._totalTime = None
+     
+  def totalTime(self):
+    if self._totalTime is None:
+      def parseMark(mark):
+        return datetime(1,1,1,*map(int, mark.split('.')))
+      times = list(map(parseMark, self.timeMarks))
+      pairs = zip(times[0::2], times[1::2])
+      sums = map(lambda pair: (pair[1] - pair[0]).seconds / 60, pairs)
+      self._totalTime = sum(sums) / 60
+    return self._totalTime
+    
   def fromLine(line):
     split = line.split('-')
     taskName = split.pop().strip()
     timeMarks = [timeMark.strip() for item in split for timeMark in item.split(',')]
     return Task(taskName, timeMarks)
-    
-  def totalTime
-
+      
 def startNewDay(day, days):
   if day is not None:
     days.append(day)  
   return []
-
 
 days = []
 day = None
@@ -39,4 +48,5 @@ for day in days:
     print(task.name)
     print(task.timeMarks)
     print(task.issues)
+    print(task.totalTime())
   print('===================')
